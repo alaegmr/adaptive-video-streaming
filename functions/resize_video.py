038d45f44123a0c6ca71c8bccf0e2ac98c1d3f50
Alae Ghomari <149176753+alaegmr@users.noreply.github.com>
2025-12-02 13:35:06 -0800

    Refactor video resizing functions
    Refactor video resizing to use subprocess for ffmpeg commands.

--- functions/resize_video.py
+++ functions/resize_video.py
@@ -1,26 +1,16 @@
-import os, threading
-from .ffmpeg_utils import fast_convert
+import subprocess
 
-def create_resolutions(segment_path, resized_folder):
-    """Crée toutes les versions d’un segment"""
-    resolutions = {
-        "360p": "640x360",
-        "480p": "854x480",
-        "720p": "1280x720",
-        "1080p": "1920x1080",
-        "4k": "3840x2160"
-    }
+def run_ffmpeg(cmd):
+    """Exécute une commande ffmpeg"""
+    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    versions = {}
-    threads = []
-
-    for label, res in resolutions.items():
-        out_name = f"{os.path.splitext(os.path.basename(segment_path))[0]}_{label}.mp4"
-        out_path = os.path.join(resized_folder, out_name)
-        t = threading.Thread(target=fast_convert, args=(segment_path, out_path, res))
-        t.start()
-        threads.append(t)
-        versions[label] = f"static/segments_resized/{out_name}"
-
-    for t in threads: t.join()
-    return versions
+def fast_convert(input_path, output_path, resolution):
+    """Convertit une vidéo à une résolution spécifique"""
+    run_ffmpeg([
+        "ffmpeg", "-y", "-i", input_path,
+        "-vf", f"scale={resolution}",
+        "-c:v", "libx264",
+        "-preset", "ultrafast",
+        "-crf", "23",
+        output_path
+    ])
